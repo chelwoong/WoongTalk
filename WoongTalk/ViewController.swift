0de680506a5491371aa77cdc0c559c33fadfbe18
woongs <ccw1021@naver.com>
2018-11-05 16:59:13 +0900

    connect firebaseConfig (make loading view)

--- WoongTalk/ViewController.swift
+++ WoongTalk/ViewController.swift
@@ -7,14 +7,90 @@
 //
 
 import UIKit
+import SnapKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    var box = UIImageView()
+    var remoteConfig: RemoteConfig!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 서버 연결
+        remoteConfig = RemoteConfig.remoteConfig()
+        remoteConfig.configSettings = RemoteConfigSettings(developerModeEnabled: true)
+        remoteConfig.setDefaults(fromPlist: "RemoteConfigDefaults")
+        
+        
+        // fetch. 서버값 받아오기
+        // TimeInterval 0으로 하면 앱을 킬 때마다 받아옴, 3600은 한 시간마다
+        remoteConfig.fetch(withExpirationDuration: TimeInterval(0)) { (status, error) -> Void in
+            if status == .success {
+                print("Config fetched!")
+                self.remoteConfig.activateFetched()
+            } else {
+                print("Config not fetched")
+                print("Error: \(error?.localizedDescription ?? "No error available.")")
+            }
+            // 성공하든 실패하는 기능은 해야함.
+            self.displayWelcome()
+        }
+        
+        
+        // image 센터
+        self.view.addSubview(box)
+        box.snp.makeConstraints { (make) in
+            make.center.equalTo(self.view)
+        }
+        box.image = #imageLiteral(resourceName: "loading")
     }
-
-
+    
+    func displayWelcome() {
+        
+        let color = remoteConfig["splash_background"].stringValue
+        let caps = remoteConfig["splash_message_caps"].boolValue
+        let message = remoteConfig["splash_message"].stringValue
+        
+        if(caps) {
+            
+            let alert = UIAlertController(title: "공지사항", message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: { (action) in
+                exit(0)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        
+        }
+        self.view.backgroundColor = UIColor(hex: color!)
+    }
 }
+
+// Hex 칼라를 이용하기 위한 extension
+extension UIColor {
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        
+        // 0으로 하면 #aabbcc 같이 있을 때 #부터 읽어버리기 때문에 1로 수정
+        scanner.scanLocation = 1
+        
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
+}
+
+
+
+
 
